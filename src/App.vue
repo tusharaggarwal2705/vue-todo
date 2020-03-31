10d5a617b3edf2971e3fb6a60732506ff90f620e
tushar <tushar@metadesignsolutions.co.uk>
2020-03-31 12:31:24 +0530

    created todo and todositem component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    
+    <Todos v-bind:todos="todos" />
     
   </div>
 </template>
 
 <script>
-
+import Todos from './components/Todos'
 
 export default {
   name: 'App',
   components: {
-    
+    Todos
+  },
+  data(){
+    return {
+      todos:[
+        {
+          _id:1,
+          title:'create user-interface',
+          isCompleted:false
+        },
+        {
+          _id:2,
+          title:'create back-end',
+          isCompleted:false
+        },
+        {
+          _id:3,
+          title:'create front-end',
+          isCompleted:false
+        }
+      ]
+    }
   }
 }
 </script>
